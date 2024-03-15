ddeedf1676c1649b67272d76b6a788384aa07d9b
Pedro Lisboa <pedrogabriel3@hotmail.com>
2024-03-15 03:41:21 -0300

    Update package versions in pubspec.yaml

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:seduc_app/main.dart';
+import 'package:appSchool/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
